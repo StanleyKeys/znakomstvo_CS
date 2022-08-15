55b403ecdf98e286585820968885ab63e654b4a0
StanleyKeys <108968816+StanleyKeys@users.noreply.github.com>
2022-08-15 14:18:47 +0700

    Task_29_1
    Added Task_29_1

---
+++ Task_29_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int size = 8;
+int[] array = new int[8];
+ 
+    Console.WriteLine("Вывод массивa из 8 элементов");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(0, 8);
+            Console.Write($"{array[i]} ");
+        }
