01ab847c490ac25283d07166b49b263b31fd6f12
StanleyKeys <108968816+StanleyKeys@users.noreply.github.com>
2022-08-27 19:28:21 +0700

    Task_50
    Added Task_50 Seminar_07

---
+++ Task_50/Program.cs
@@ -0,0 +1,110 @@
+﻿/* Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+    Например, задан массив:
+    1 4 7 2
+    5 9 2 3
+    8 4 2 4
+    17 -> такого числа в массиве нет
+*/
+
+// код из задачи 47: необходим для заполнения двумерного массива.
+Console.Clear();
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+double[,] array = new double[m, n];
+
+Console.WriteLine();
+
+void FillArray(double[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+
+void PrintArray (double[,] array)
+{
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        double alignNumber = Math.Round(array[i, j], 1);
+        Console.Write(alignNumber + " ");
+      }
+      Console.WriteLine();
+  }
+}
+Console.WriteLine("Ваш двумерный массив: ");
+FillArray(array);
+PrintArray(array);
+
+
+// код для нахождения значения элемента по заданным координатам:
+
+Console.Write("\nВведите координаты позиции элемента, разделенных запятой: ");
+
+string? positionElement = Console.ReadLine();
+positionElement = RemovingSpaces(positionElement);
+int[] position = ParserString(positionElement);
+
+if(position[0] <= m 
+&& position[1] <= n 
+    && position[0] >= 0 
+    && position[1] >= 0)
+{
+  double result = array[position[0]-1, position[1]-1];
+  Console.Write($"Значение элемента: {result}");
+}
+else Console.Write($"такого элемента в массиве нет.");
+
+int[] ParserString(string input)
+{
+  int countNumbers = 1;
+  for (int i = 0; i < input.Length; i++)
+  {
+    if (input[i] == ',')
+    countNumbers++;
+  }
+
+  int[] numbers = new int[countNumbers];
+
+  int numberIndex = 0;
+  for(int i = 0; i < input.Length; i++)
+  {
+    string subString = String.Empty;
+
+    while (input[i] != ',')
+    {
+        subString += input[i].ToString();
+        if (i >= input.Length - 1)
+        break;
+        i++;
+    }
+
+    numbers[numberIndex] = Convert.ToInt32(subString);
+    numberIndex++;
+  }
+
+  return numbers;
+}
+
+string RemovingSpaces(string input)
+{
+  string output = String.Empty;
+  for (int i = 0; i < input.Length; i++)
+  {
+    if (input[i] != ' ') 
+    {
+      output += input[i];
+    }
+  }
+  return output;
+}
