8306ebab1f9214bf3733881f7bd790b706138bcc
StanleyKeys <108968816+StanleyKeys@users.noreply.github.com>
2022-08-27 19:29:02 +0700

    Task_52
    Added Task_52 Seminar_07

---
+++ Task_52/Program.cs
@@ -0,0 +1,92 @@
+﻿/* Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+    Например, задан массив:
+    1 4 7 2
+    5 9 2 3
+    8 4 2 4
+    Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+*/
+
+Console.Clear();
+
+// код из Задачи 47:
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+double[,] array = new double[m, n];
+Console.WriteLine();
+
+void FillArray(double[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+
+void PrintArray (double[,] array)
+{
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        double alignNumber = Math.Round(array[i, j], 1);
+        int array1 = Convert.ToInt32(alignNumber); // конвертируем из double в int 
+        Console.Write(array1 + " ");
+      }
+      Console.WriteLine();
+  }
+}
+Console.WriteLine("Ваш двумерный массив: ");
+FillArray(array);
+PrintArray(array);
+
+
+
+int[,] arrayWhole = new int[m, n];
+arrayWhole = ArrayBox(array);
+
+// код на среднее арифметическое:
+
+Console.Write($"\nCреднее арифметическое столбца:\n");
+for (int i = 0; i < n; i++)
+{
+  double arithmeticMean = 0;
+  for (int j = 0; j < m; j++)
+  {
+    arithmeticMean += arrayWhole[j, i];
+  }
+  arithmeticMean = Math.Round(arithmeticMean / m, 1);
+  Console.WriteLine($"№ {i+1} = {arithmeticMean}");
+}
+
+int[,] ArrayBox (double[,] array)
+{
+  int[,] arrayWhole = new int[array.GetLength(0), array.GetLength(1)];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      arrayWhole[i, j] = Convert.ToInt32(array[i, j]);
+    }
+  }
+  return arrayWhole;
+}
+
+void WriteArrayInt (int[,] arrayWhole)
+{
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        Console.Write(arrayWhole[i, j] + " ");
+      }
+      Console.WriteLine();
+  }
+}
